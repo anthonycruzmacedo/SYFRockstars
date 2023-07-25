9ee36d2bc7bb7acd9f2f82686e7808075ab0d80b
anthony <anthony.cruzmacedo@protonmail.com>
2023-07-25 15:12:36 -0500

    initialize Customer Payment menu for demo

--- verpay/src/App.js
+++ verpay/src/App.js
@@ -8,9 +8,16 @@ import {
   Card,
   CardContent,
   Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Button,
 } from "@mui/material";
 
 function App() {
+  const names = ["Harsh", "Grant", "Madison", "Shubham", "Anthony"];
+  const fieldTitle = ["Cardholder Name", "Store", "Location", "Date", "Amount"];
+
   return (
     <div className="h-screen w-screen bg-[#34657F] flex flex-row">
       {/* iPhone content div */}
@@ -27,14 +34,39 @@ function App() {
         <div className="bg-white h-[525px] w-[250px] rounded-[15%] z-0"></div>
       </div>
 
-      <div className="w-[350px] flex justify-center items-center ml-[10%]">
-        <Card className="h-3/5 w-4/5 shadow-black shadow-xl p-5 !rounded-xl">
+      <div className="w-[350px] flex flex-col items-center justify-center ml-[10%]">
+        <Card className="h-4/5 w-4/5 shadow-black shadow-xl !rounded-xl flex flex-col items-center justify-evenly">
+          {/* Control Title */}
           <Typography>
-            <h1 className="font-bold font-sans">Customer Payment Info</h1>
+            <h1 className="pl-4 pt-4 pb-0 mb-0 font-bold font-sans">
+              Customer Payment Info
+            </h1>
           </Typography>
-          <List>
-            <ListItem></ListItem>
+
+          <List className="">
+            {/* <p className="pl-4">Cardholder Name</p>
+            <ListItem>
+              <FormControl className="w-full">
+                <InputLabel className="bg-white">Cardholder Name</InputLabel>
+                <Select className="!py-0 !my-0 h-[36px]" props={names}>
+                  {names.map((x) => (
+                    <MenuItem value={x}>{x}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </ListItem> */}
+            {fieldTitle.map((title) => (
+              <div className="mb-8 h-[36px]">
+                <p className="pl-4">{title}</p>
+                <ListItem className="w-full !py-0">
+                  <TextField size="small" className="w-full !py-0 h-[12px]" />
+                </ListItem>
+              </div>
+            ))}
           </List>
+          <Button className=" !bg-blue-300 border-sm pb-4">
+            Initiate Payment
+          </Button>
         </Card>
       </div>
     </div>
